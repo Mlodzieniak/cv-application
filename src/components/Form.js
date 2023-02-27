73dcd14f67b25323899ea7df2d2a41302bc9d445
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-02-27 23:14:27 +0100

    created components for app structure

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+class Form extends React.Component{
+  render(){ return (
+    <div className="overview">
+     
+    </div>
+  );}
+ 
+}
+
+export default Form;
