511ec52895fb4f090300d26f4397d69abc39776c
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-02-27 23:07:31 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './styles/logo.svg';
+import React from 'react';
+import './App.css';
+
+class App extends React.Component{
+  render(){ return (
+    <div className="App">
+     
+    </div>
+  );}
+ 
+}
+
+export default App;
