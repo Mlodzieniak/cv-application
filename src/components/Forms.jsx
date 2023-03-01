bbe106011eee8a093497c4b32043f5243ffe819f
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-03-01 10:26:13 +0100

    working form slider

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -4,16 +4,51 @@ import EducationForm from "./EducationForm";
 import JobExpForm from "./JobExpForm";
 
 class Forms extends React.PureComponent {
+  constructor() {
+    super();
+    this.moveLeft = this.moveLeft.bind(this);
+    this.moveRight = this.moveRight.bind(this);
+
+    this.state = {
+      left: 0,
+    };
+  }
+
+  moveLeft() {
+    this.setState((prevState) => ({ left: prevState.left + 50 }));
+    if (this.state.left >= 0) {
+      this.setState({
+        left: 0,
+      });
+    }
+  }
+
+  moveRight() {
+    this.setState((prevState) => ({ left: prevState.left - 50 }));
+    if (this.state.left <= -100) {
+      this.setState({
+        left: -100,
+      });
+    }
+  }
+
   render() {
+    const { left } = this.state;
     return (
       <div className="forms bg-white">
-        <div className="slider flex-row">
+        <div
+          style={{ transform: `translateX(${left}vw)` }}
+          className="slider flex-row"
+        >
           <PersonalDataForm />
           <EducationForm />
           <JobExpForm />
         </div>
         <div className="nav-buttons flex-row space-between align-center bold">
-          <nav className="prev font2 bg-yellow rounded-button shadow">
+          <nav
+            onClick={this.moveLeft}
+            className="prev font2 bg-yellow rounded-button shadow"
+          >
             <ion-icon name="chevron-back-outline" />{" "}
           </nav>
           <nav className="bg-yellow shadow radius">
@@ -21,7 +56,10 @@ class Forms extends React.PureComponent {
               Apply
             </button>
           </nav>
-          <nav className="next font2 bg-yellow rounded-button shadow">
+          <nav
+            onClick={this.moveRight}
+            className="next font2 bg-yellow rounded-button shadow"
+          >
             <ion-icon name="chevron-forward-outline" />{" "}
           </nav>
         </div>
