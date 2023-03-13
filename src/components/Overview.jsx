dcf6ea9a484ecd9f5853043b2724b0f2144ecf85
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-03-13 18:01:07 +0100

    all components as functions

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -3,78 +3,77 @@ import React from "react";
 import propTypes from "prop-types";
 import uniqid from "uniqid";
 
-class Overview extends React.PureComponent {
-  render() {
-    const { personalData, education, jobs } = this.props;
-    return (
-      <div className="overview bg-white">
-        <div className="names">
-          <h1>{personalData.name}</h1>
-          <h1>{personalData.surname}</h1>
-        </div>
-        <div className="adress">
-          <h3>Address</h3>
-          <p>{personalData.city}</p>
-          <p>{personalData.street}</p>
-        </div>
+function Overview(props) {
+  const { personalData, education, jobs } = props;
 
-        <div className="phone">
-          <h3>Tel </h3>
-          <p>{personalData.tel}</p>
-        </div>
-        <div className="email">
-          <h3>E-mail </h3>
-          <p>{personalData.email}</p>
-        </div>
-        <div className="linked">
-          <h3>Linked</h3>
-          <p>{personalData.linked}</p>
-        </div>
-        <p>{personalData.about}</p>
-
-        <div className="education">
-          <h3 className="span">Education</h3>
-
-          {education.map((school) => (
-            <div className="school" key={uniqid()}>
-              <p className="cv-years bold">
-                {school.start} - {school.end}
-              </p>
-              <div>
-                <p className="bold">{school.name}</p>
-                <p className="">{school.fos}</p>
-                <p className="">{school.degree}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="experience">
-          <h3 className="span">Experience</h3>
-          <span />
-          {jobs.map((job) => (
-            <div className="job" key={uniqid()}>
-              {" "}
-              <p className="cv-years bold">
-                {job.start} - {job.end}
-              </p>
-              <div className="job-main">
-                <p className="bold">{job.company}</p>
-
-                <p className="">{job.position}</p>
-                <ul>
-                  {job.description.map((desc) => (
-                    <li key={uniqid()} className="duty">
-                      {desc.text}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ))}
-        </div>
+  return (
+    <div className="overview bg-white">
+      <div className="names">
+        <h1>{personalData.name}</h1>
+        <h1>{personalData.surname}</h1>
       </div>
-    );
-  }
+      <div className="adress">
+        <h3>Address</h3>
+        <p>{personalData.city}</p>
+        <p>{personalData.street}</p>
+      </div>
+
+      <div className="phone">
+        <h3>Tel </h3>
+        <p>{personalData.tel}</p>
+      </div>
+      <div className="email">
+        <h3>E-mail </h3>
+        <p>{personalData.email}</p>
+      </div>
+      <div className="linked">
+        <h3>Linked</h3>
+        <p>{personalData.linked}</p>
+      </div>
+      <p>{personalData.about}</p>
+
+      <div className="education">
+        <h3 className="span">Education</h3>
+
+        {education.map((school) => (
+          <div className="school" key={uniqid()}>
+            <p className="cv-years bold">
+              {school.start} - {school.end}
+            </p>
+            <div>
+              <p className="bold">{school.name}</p>
+              <p className="">{school.fos}</p>
+              <p className="">{school.degree}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="experience">
+        <h3 className="span">Experience</h3>
+        <span />
+        {jobs.map((job) => (
+          <div className="job" key={uniqid()}>
+            {" "}
+            <p className="cv-years bold">
+              {job.start} - {job.end}
+            </p>
+            <div className="job-main">
+              <p className="bold">{job.company}</p>
+
+              <p className="">{job.position}</p>
+              <ul>
+                {job.description.map((desc) => (
+                  <li key={uniqid()} className="duty">
+                    {desc.text}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 Overview.defaultProps = {
   personalData: {
--- src/components/PersonalDataForm.jsx
+++ src/components/PersonalDataForm.jsx
@@ -1,150 +1,192 @@
 /* eslint-disable react/destructuring-assignment */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import propTypes from "prop-types";
 
-class PersonalDataForm extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      surname: "",
-      email: "",
-      tel: "",
-      city: "",
-      street: "",
-      linked: "",
-      about: "",
-    };
-  }
+function PersonalDataForm(props) {
+  const { onChange } = props;
 
-  componentDidMount() {
-    if ("personalData" in localStorage) {
-      const data = JSON.parse(localStorage.getItem("personalData"));
-      this.setState(
-        {
-          name: data.name,
-          surname: data.surname,
-          email: data.email,
-          tel: data.tel,
-          city: data.city,
-          street: data.street,
-          linked: data.linked,
-          about: data.about,
-        },
-        () => this.props.onChange(this.state, "personalData")
-      );
-    }
-  }
-
-  handleChange = (event, key) => {
-    this.setState(
-      {
-        [key]: event.target.value,
-      },
-      () => {
-        this.props.onChange(this.state, "personalData");
-        localStorage.setItem("personalData", JSON.stringify(this.state));
-      }
-    );
+  const propCheck = (type) => {
+    const data = JSON.parse(localStorage.getItem("personalData"));
+    return data[type] ? data[type] : "";
   };
+  const [name, setName] = useState(propCheck("name"));
+  const [surname, setSurname] = useState(propCheck("surname"));
+  const [email, setEmail] = useState(propCheck("email"));
+  const [tel, setTel] = useState(propCheck("tel"));
+  const [city, setCity] = useState(propCheck("city"));
+  const [street, setStreet] = useState(propCheck("street"));
+  const [linked, setLinked] = useState(propCheck("linked"));
+  const [about, setAbout] = useState(propCheck("about"));
 
-  render() {
-    return (
-      <div className="form">
-        <form className="flex-col" method="get">
-          <div className="input">
-            <label htmlFor="name">
-              <div className="label">Name:</div>
-              <input
-                onChange={(event) => this.handleChange(event, "name")}
-                value={this.state.name}
-                id="name"
-                type="text"
-              />
-            </label>
-          </div>
-          <div className="input">
-            <label htmlFor="surname">
-              Surname:
-              <input
-                onChange={(event) => this.handleChange(event, "surname")}
-                value={this.state.surname}
-                id="surname"
-                type="text"
-              />
-            </label>
-          </div>
-          <div className="input">
-            <label htmlFor="email">
-              E-mail:
-              <input
-                onChange={(event) => this.handleChange(event, "email")}
-                value={this.state.email}
-                id="email"
-                type="email"
-              />
-            </label>
-          </div>
-          <div className="input">
-            <label htmlFor="phone">
-              Phone:
-              <input
-                onChange={(event) => this.handleChange(event, "tel")}
-                value={this.state.tel}
-                id="phone"
-                type="tel"
-              />
-            </label>
-          </div>
-          <div className="input">
-            <label htmlFor="city">
-              City:
-              <input
-                onChange={(event) => this.handleChange(event, "city")}
-                value={this.state.city}
-                id="city"
-                type="text"
-              />
-            </label>
-          </div>
-          <div className="input">
-            <label htmlFor="street">
-              Street:
-              <input
-                onChange={(event) => this.handleChange(event, "street")}
-                value={this.state.street}
-                id="street"
-                type="text"
-              />
-            </label>
-          </div>
-          <div className="input">
-            <label htmlFor="linked">
-              Linkedin:
-              <input
-                onChange={(event) => this.handleChange(event, "linked")}
-                value={this.state.linked}
-                id="linked"
-                type="text"
-              />
-            </label>
-          </div>
-          <div className="input">
-            <label htmlFor="about">
-              About:
-              <textarea
-                rows="7"
-                onChange={(event) => this.handleChange(event, "about")}
-                value={this.state.about}
-                id="about"
-                type="tel"
-              />
-            </label>
-          </div>
-        </form>
-      </div>
-    );
-  }
+  // constructor(props) {
+  //   super(props);
+  //    = {
+  //     name: "",
+  //     surname: "",
+  //     email: "",
+  //     tel: "",
+  //     city: "",
+  //     street: "",
+  //     linked: "",
+  //     about: "",
+  //   };
+  // }
+
+  // componentDidMount() {
+  //   if ("personalData" in localStorage) {
+  //     const data = JSON.parse(localStorage.getItem("personalData"));
+  //     this.setState(
+  //       {
+  //         name: data.name,
+  //         surname: data.surname,
+  //         email: data.email,
+  //         tel: data.tel,
+  //         city: data.city,
+  //         street: data.street,
+  //         linked: data.linked,
+  //         about: data.about,
+  //       },
+  //       () => this.props.onChange(, "personalData")
+  //     );
+  //   }
+  // }
+
+  // const changeName = (e)=>{setName(e.target.value)}
+  // const changeSurname = (e)=>{setSurname(e.target.value)}
+  // const changeEmail = (e)=>{setEmail(e.target.value)}
+  // const changeTel = (e)=>{setTel(e.target.value)}
+  // const changeCity = (e)=>{setCity(e.target.value)}
+  // const changeStreet = (e)=>{setStreet(e.target.value)}
+  // const changeLinked = (e)=>{setLinked(e.target.value)}
+  // const changeAbout = (e)=>{setName(e.target.value)}
+
+  // handleChange = (event, key) => {
+  //   this.setState(
+  //     {
+  //       [key]: event.target.value,
+  //     },
+  //     () => {
+  //       this.props.onChange(, "personalData");
+  //       localStorage.setItem("personalData", JSON.stringify());
+  //     }
+  //   );
+  // };
+  useEffect(() => {
+    const obj = { name, surname, email, tel, city, street, linked, about };
+    onChange(obj);
+    localStorage.setItem("personalData", JSON.stringify(obj));
+  }, [name, surname, email, tel, city, street, linked, about]);
+  return (
+    <div className="form">
+      <form className="flex-col" method="get">
+        <div className="input">
+          <label htmlFor="name">
+            <div className="label">Name:</div>
+            <input
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              value={name}
+              id="name"
+              type="text"
+            />
+          </label>
+        </div>
+        <div className="input">
+          <label htmlFor="surname">
+            Surname:
+            <input
+              onChange={(e) => {
+                setSurname(e.target.value);
+              }}
+              value={surname}
+              id="surname"
+              type="text"
+            />
+          </label>
+        </div>
+        <div className="input">
+          <label htmlFor="email">
+            E-mail:
+            <input
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              value={email}
+              id="email"
+              type="email"
+            />
+          </label>
+        </div>
+        <div className="input">
+          <label htmlFor="phone">
+            Phone:
+            <input
+              onChange={(e) => {
+                setTel(e.target.value);
+              }}
+              value={tel}
+              id="phone"
+              type="tel"
+            />
+          </label>
+        </div>
+        <div className="input">
+          <label htmlFor="city">
+            City:
+            <input
+              onChange={(e) => {
+                setCity(e.target.value);
+              }}
+              value={city}
+              id="city"
+              type="text"
+            />
+          </label>
+        </div>
+        <div className="input">
+          <label htmlFor="street">
+            Street:
+            <input
+              onChange={(e) => {
+                setStreet(e.target.value);
+              }}
+              value={street}
+              id="street"
+              type="text"
+            />
+          </label>
+        </div>
+        <div className="input">
+          <label htmlFor="linked">
+            Linkedin:
+            <input
+              onChange={(e) => {
+                setLinked(e.target.value);
+              }}
+              value={linked}
+              id="linked"
+              type="text"
+            />
+          </label>
+        </div>
+        <div className="input">
+          <label htmlFor="about">
+            About:
+            <textarea
+              rows="7"
+              onChange={(e) => {
+                setAbout(e.target.value);
+              }}
+              value={about}
+              id="about"
+              type="tel"
+            />
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 }
 PersonalDataForm.propTypes = {
   onChange: propTypes.func.isRequired,
